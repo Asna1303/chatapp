5fcc554d69674d076d76bdf229aeb6311579a272
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-20 23:45:42 +0530

    search component is done

--- src/molecules/homepage.jsx
+++ src/molecules/homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Chatting from '../components/Chatting'
+
 const  Home =() =>{
   return (
     <div className='home'>
         <div className="container">
             <Sidebar/>
             <Chatting/>
+          
         </div>
         </div>
   )
