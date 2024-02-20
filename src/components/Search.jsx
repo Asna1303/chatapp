3bff3677c487d23f0944ee3fd8b5a05cb5ef18f1
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-20 23:21:53 +0530

    started to design search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>
-      
+    <div className='search'>
+      <div className="searchForm">
+      <input type="text"/>
+      </div>
+      <div className="userChat">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s"  alt="" />
+        <div className="userChatInfo">
+        <span>ammu</span>
+        </div>
+      </div>
     </div>
   )
 }
