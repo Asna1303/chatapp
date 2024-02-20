d16b6f05969b430636da6330387fcc4af2dbf1dc
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-20 19:00:18 +0530

    added a button for signup

--- src/molecules/registerpage.jsx
+++ src/molecules/registerpage.jsx
@@ -4,12 +4,16 @@ const Registerpage =() =>{
   return (
     <div className='form-container'>
         <div className='formWrapper'>
+            <span className="logo">Placeme Chat</span>
+            <span className="title">Register</span>
             <form>
 <input type="text" placeholder="display name"/>
 <input type="email" placeholder=" email"/>
 <input type="password" placeholder="password "/>
 <input type="file"/>
+<button>Sign Up</button>
             </form>
+            <p>Do you have an account? Login</p>
         </div>
     </div>
   )
