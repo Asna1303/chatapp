0b017a96b1895f25167d6b9f94b4bbe92fdadd20
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-21 15:43:56 +0530

    applied scrolling

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      
+    <div className='message'>
+      <div className="messageInfo">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent"></div>
+      <p>Hello</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s" alt="" />
     </div>
   )
 }
