66f19729c794ceb7585cdcb9c3d30a32858600aa
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-21 16:02:27 +0530

    row reversed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className="messageInfo">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s" alt="" />
         <span>just now</span>
