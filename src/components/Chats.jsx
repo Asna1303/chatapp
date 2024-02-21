c13499d6578e7b61a0feb97c035f633137228b24
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-21 12:22:34 +0530

    style.scss is applied and to userchat info

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const Chats = () => {
   return (
-    <div>
-      
+    <div className='chats'>
+      <div className="userChat">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s" alt="" />
+        <div className="userChatInfo">
+          <span>ammu</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s" alt="" />
+        <div className="userChatInfo">
+          <span>ammu</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chats
+export default Chats;
+
