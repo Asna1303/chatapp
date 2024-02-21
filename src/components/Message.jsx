117111a335bbef8ecbcbc9bc8486854c694f3043
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-21 15:54:02 +0530

    style.scss applied for message info and message content

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -9,7 +9,7 @@ const Message = () => {
       </div>
       <div className="messageContent"></div>
       <p>Hello</p>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s" alt="" />
+      {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s" alt="" /> */}
     </div>
   )
 }
