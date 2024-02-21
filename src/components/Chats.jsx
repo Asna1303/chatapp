6e6a0674f3e7b18a488aec10131109e08d0afe48
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-21 12:50:41 +0530

    added images for add,cam ,more and styled them

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -17,6 +17,13 @@ const Chats = () => {
           <p>Hello</p>
         </div>
       </div>
+      <div className="userChat">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7W2FEkF8g9XbhOrCVezIm6_e1n_GVCFCNpHom2Sh9Q&s" alt="" />
+        <div className="userChatInfo">
+          <span>ammu</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   );
 }
