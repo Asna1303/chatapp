61fb0409ea7d14ca3a43ca37940655b0d846f091
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-22 16:02:58 +0530

    started to add data to firebase

--- src/molecules/registerpage.jsx
+++ src/molecules/registerpage.jsx
@@ -1,79 +1,70 @@
 import React, { useState } from "react";
 import Add from "../img/addavatar.png"
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth ,storage,db} from "../firebase";
+import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 
-const Registerpage =() =>{
-  const {err,setErr}=useState(false)
-const handleSubmit = async(e) =>{
-  e.preventDefault()
-  const displayName = e.target[0].value;
-  const email = e.target[1].value;
-  const password = e.target[2].value;
-  const file = e.target[3].files[0];
-try{
-const res= createUserWithEmailAndPassword(auth, email, password)
+const Registerpage = () => {
+  const [err, setErr] = useState(false);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+    
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const storageRef = ref(storage, displayName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
 
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+    }
+  };
 
-const storageRef = ref(storage, displayName);
-
-const uploadTask = uploadBytesResumable(storageRef, file);
-
-// Register three observers:
-
-uploadTask.on(
-  
-  (error) => {
-    setErr(true);
-    // Handle unsuccessful uploads
-  }, 
-  () => {
-    // Handle successful uploads on complete
-    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-      await updateProfile(res.user, {
-        displayName,
-        photoURL: downloadURL,
-      });
-      await setDoc(doc(db, "users", res.user.uid), {
-        uid: res.user.uid,
-        displayName,
-        email,
-        photoURL: downloadURL,
-      });
-    });
-  }
-);
-
-}catch(err){
-  setErr(true);
-} 
-
-};
   return (
     <div className='formContainer'>
-        <div className='formWrapper'>
-            <span className="logo">Placeme Chat</span>
-            <span className="title">Register</span>
-            <form onSubmit={handleSubmit}>
-<input type="text" placeholder="display name"/>
-<input type="email" placeholder=" email"/>
-<input type="password" placeholder="password "/>
-<input style={{display:"none"}} type="file" id="file"/>
-<label htmlFor="file">
-    <img src={Add} alt="" />
-    <span>Add an avatar</span>
-</label>
-<button>Sign Up</button>
-{err && <span>Something went wrong</span>}
-            </form>
-            <p>Do you have an account? Login</p>
-        </div>
+      <div className='formWrapper'>
+        <span className="logo">Placeme Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder=" email" />
+          <input type="password" placeholder="password " />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>Do you have an account? Login</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Registerpage
+export default Registerpage;
