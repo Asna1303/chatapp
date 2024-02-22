0772f7a7cb41bfade30ef8ef7a0f14d6f27246a8
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-02-22 19:02:35 +0530

    registerpage is ready

--- src/molecules/homepage.jsx
+++ src/molecules/homepage.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Chatting from '../components/Chatting'
 
-const  Home =() =>{
+const Home = () => {
   return (
     <div className='home'>
-        <div className="container">
-            <Sidebar/>
-            <Chatting/>
-          
-        </div>
-        </div>
+      <div className="container">
+        <Sidebar/>
+        <Chatting/>
+      </div>
+    </div>
   )
 }
 
